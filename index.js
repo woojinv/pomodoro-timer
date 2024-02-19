a57a66949ea0a8e52886a40990d36c63deaa2dee
woojinv <oh.woojin95@gmail.com>
2024-02-19 10:50:18 -0600

    alter css classes to toggle visibility

--- index.js
+++ index.js
@@ -1,52 +1,22 @@
 // Constants
-const pomodoroSeconds = 5;
-const shortBreakSeconds = 6;
-const longBreakSeconds = 7;
 
 const pomodoroModeEl = document.getElementById('pomodoroMode');
 const shortBreakModeEl = document.getElementById('shortBreakMode');
 const longBreakModeEl = document.getElementById('longBreakMode');
 
-const timerEl = document.getElementById('timer');
+const pomodoroStartButton = document.getElementById('pomodoroStartButton');
 
-const startButton = document.getElementById('startButton');
+const pomodoroStopButton = document.getElementById('pomodoroStopButton');
 
-pomodoroModeEl.addEventListener('change', function () {
-  timerEl.innerHTML = getFormattedTime(pomodoroSeconds);
+pomodoroStartButton.addEventListener('click', function () {
+  hide(pomodoroStartButton);
+  show(pomodoroStopButton);
 });
 
-shortBreakModeEl.addEventListener('change', function () {
-  timerEl.innerHTML = getFormattedTime(shortBreakSeconds);
-});
-
-startButton.addEventListener('click', function () {
-  let totalSeconds = getTotalSeconds();
-
-  const timer = setInterval(function () {
-    totalSeconds -= 1;
-    timerEl.innerHTML = getFormattedTime(totalSeconds);
-  }, 1000);
-});
-
-function getFormattedTime(seconds) {
-  const newMinutes = Math.floor(seconds / 60);
-  const formattedMinutes = newMinutes < 10 ? '0' + newMinutes : newMinutes;
-  const newSeconds = seconds % 60;
-  const formattedSeconds = newSeconds < 10 ? '0' + newSeconds : newSeconds;
-  return `${formattedMinutes}:${formattedSeconds}`;
+function hide(domEl) {
+  domEl.classList.remove('visible');
 }
 
-function getTotalSeconds() {
-  const timeValues = timerEl.innerHTML.split(':');
-  const minutes = Number(timeValues[0]);
-  const seconds = Number(timeValues[1]);
-  return minutes * 60 + seconds;
-}
-// What if i click a mode button while a countodwn for that mode is happening?
-
-// while a countdown is occurring, if a user switches to a different mode,
-// should the countdown keep happening in the background?
-
-class Timer {
-  
+function show(domEl) {
+  domEl.classList.add('visible');
 }
